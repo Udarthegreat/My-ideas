42d4e994e2f1bee169fffff4e23f05a05a9cf34e
Udarthegreat <radu.g.habinyak@gmail.com>
2019-07-10 12:34:11 -0400

    done with this  page, will fix latter

--- redoing the statick page for earth 2.0/cloth_sim.js
+++ redoing the statick page for earth 2.0/cloth_sim.js
@@ -6,7 +6,7 @@ grd.addColorStop(0, "red");
 grd.addColorStop(1, "white");
 // Fill with gradient
 ctx.fillStyle = grd;
-ctx.fillRect(0, 0, 600, 400);
+ctx.fillRect(0, 0, 600, 300);
 //need to get the following to render on top of the gradient
 ctx.font = "30px Arial";
-ctx.fillText("Hello World", 200, 200);
+ctx.fillText("Hello World", 200, 350);
